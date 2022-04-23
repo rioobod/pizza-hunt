820710ca5044621459e74eed8ac836a99203d748
rioobod <akanbilola@gmail.com>
2022-04-23 12:55:53 -0500

    update routes

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,7 @@ router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
 });
 
+const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-routes');
+
 module.exports = router;
